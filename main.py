fd56fa1ab0edca1d6a7f08268e42e96b1e5f6bc8
Krish Patel <79666419+Krishpkreame@users.noreply.github.com>
2022-09-05 00:23:10 +1200

    Big Change - Flask
    Using Flask I did not need to encrypt the user name and password because I had a static webpage to use. I will host this on my server for user feedback and so I can work off it better

--- main.py
+++ main.py
@@ -1,30 +1,21 @@
-import re
 from kamarapi import *
-from encrypt import local_encryption
 from filterapi import filter_kamar
-if __name__ == "__main__":
-    cryt = local_encryption()
-    while True:
-        if input("New user? [y/n]") == "y":
-            temp1 = input("what school u goto? \n").lower() + "\n"
-            for i in range(0, 50):
-                print("")
-            temp1 += input("username : \n").lower() + "\n"
-            for i in range(0, 50):
-                print("")
-            temp1 += input("password : \n")
-            for i in range(0, 50):
-                print("")
-            cryt.encrypt(temp1.encode("utf-8"))
-            break
-        else:
-            with open("secret.dll", "wb") as file:
-                file.write(
-                    b"gAAAAABjD9uqq5hAe6_bgLyH7j-GZ-h6rcw8aRmhaXpGWKVLg-vAn-p8gTOdP-dOcgMsVyHkENnUqzF2CY2g7ZcHTZYejUrEYG579TgegzKroBOHjWMXU9Y=")
-            break
+from flask import Flask, render_template, request
 
-    cred_details = cryt.decryptlogins()
-    k = kamar_api(cred_details[0], cred_details[1], cred_details[2])
+app = Flask(__name__)
+
+
+@app.route('/')
+def my_form():
+    return render_template('home.html')
+
+
+@app.route('/', methods=['POST'])
+def my_form_post():
+    k = kamar_api(
+        request.form['school'],
+        request.form['username'],
+        request.form['password'])
     results = k.getresults()
     print("-----------\n", results, "\n-------------")
     print(k.student_id, " :\n", len(results))
@@ -40,3 +31,9 @@ if __name__ == "__main__":
                 continue
             sum += i[x][2]
     print("rankscore: ", sum)
+
+    return "rankscore: " + str(sum)
+
+
+if __name__ == '__main__':
+    app.run()
---
+++ old_main.py
@@ -0,0 +1,41 @@
+from kamarapi import *
+from encrypt import local_encryption
+from filterapi import filter_kamar
+if __name__ == "__main__":
+    cryt = local_encryption()
+    while True:
+        if input("New user? [y/n]") == "y":
+            temp1 = input("what school u goto? \n").lower() + "\n"
+            for i in range(0, 50):
+                print("")
+            temp1 += input("username : \n").lower() + "\n"
+            for i in range(0, 50):
+                print("")
+            temp1 += input("password : \n")
+            for i in range(0, 50):
+                print("")
+            cryt.encrypt(temp1.encode("utf-8"))
+            break
+        else:
+            with open("secret.dll", "wb") as file:
+                file.write(
+                    b"gAAAAABjD9uqq5hAe6_bgLyH7j-GZ-h6rcw8aRmhaXpGWKVLg-vAn-p8gTOdP-dOcgMsVyHkENnUqzF2CY2g7ZcHTZYejUrEYG579TgegzKroBOHjWMXU9Y=")
+            break
+
+    cred_details = cryt.decryptlogins()
+    k = kamar_api(cred_details[0], cred_details[1], cred_details[2])
+    results = k.getresults()
+    print("-----------\n", results, "\n-------------")
+    print(k.student_id, " :\n", len(results))
+
+    filterObj = filter_kamar(open("uestandards.json").read())
+    sortedKamar = filterObj.filter_results(results)
+    # print(sortedKamar)
+    sum = 0
+    for i in sortedKamar.values():
+        print(i, "\n\n")
+        for x in i.keys():
+            if x == "subject_credits_earned":
+                continue
+            sum += i[x][2]
+    print("rankscore: ", sum)
