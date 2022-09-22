f95b1ea6e80478fbfb3bb2804036ff81075f9ea0
Krishpkreame <krishpkreame1405+github@gmail.com>
2022-09-23 02:25:08 +1200

    Progress on rest api with Express
    I used xml to js package to change the KAMAR API responses from XML to JS objects, I added correct and useful Http status codes, that return with a error msg. Using the KAMAR API I first got all the users results and compared them to the UE approved list I made. Therefore only ue approved subjects were counted. By doing this I was able filter the list and add 2 key data points where were the Rankscore contribution and the Subject the standard was for.

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
+const fs = require("fs");
 const express = require("express");
-const cors = require("cors");
 const axios = require("axios");
+const cors = require("cors");
+const X2JS = require("x2js");
 
+const ue_approved_subjects = JSON.parse(fs.readFileSync("ue_approved.json"));
 const PORT = 8080;
+const dict_school_urls = {
+  mags: "https://kamarportal.mags.school.nz/api/api.php",
+  mrgs: "https://kamarportal.mrgs.school.nz/api/api.php",
+  lynf: "https://lynfield.mystudent.school.nz/api/api.php",
+};
 
 const app = express();
+const x2js = new X2JS();
 
 const kamarheaders = {
   "Content-Type": "application/x-www-form-urlencoded",
@@ -15,42 +24,124 @@ const kamarheaders = {
 
 app.use(express.json());
 
-// GET methods API
+// ------------------ GET methods API ------------------
 app.get("/api/v2/auth", (req, res) => {
-  res.json({ error: "Auth Not implemented yet - Krish" });
+  res.json({ error: "unable to Auth with GET, use POST" });
 });
 
 app.get("/api/v2/results", (req, res) => {
-  res.json({ error: "Results Not implemented yet - Krish" });
+  res.json({ error: "unable to GET results, use POST" });
 });
 
-// POST methods API
+// ------------------ POST methods API ------------------
 app.post("/api/v2/auth", async (req, res) => {
   const req_data = req.body;
-  console.log(req_data);
 
-  if (!req_data.schoolurl || !req_data.username || !req_data.password) {
-    return res.sendStatus(400);
+  if (!req_data.school || !req_data.username || !req_data.password) {
+    return res.status(400).send({ error: "KAMAR LOGINS : `school`, `username` and `password` is required" });
   }
-
+  if (!(req_data.school in dict_school_urls)) {
+    return res
+      .status(400)
+      .send({ error: `'${req_data.school}' is not valid. Valid schools are [${Object.keys(dict_school_urls)}]` });
+  }
   var authconfig = {
     method: "post",
-    url: req_data.schoolurl,
+    url: dict_school_urls[req_data.school.toLowerCase()],
     headers: kamarheaders,
     data: `Command=Logon&Key=vtku&Username=${req_data.username}&Password=${req_data.password}`,
   };
 
   await axios(authconfig)
     .then(function (response) {
-      console.log(response.data);
-      res.sendStatus(200);
+      var responseObj = x2js.xml2js(response.data).LogonResults;
+      if (responseObj.Error) {
+        return res.status(401).send({ error: responseObj.Error });
+      }
+      res.status(200).send({
+        authkey: responseObj.Key,
+        student_id: responseObj.CurrentStudent,
+      });
     })
     .catch(function (error) {
       console.log(error);
-      res.sendStatus(400);
+      res.status(500).send({ error: "Server might be down, sorry :/ - from auth" });
     });
+});
 
-  //{ error: "WIP, returning values back", ...res });
+app.post("/api/v2/results", async (req, res) => {
+  const req_data = req.body;
+  if (!req_data.school || !req_data.student_id || !req_data.authkey) {
+    return res.status(400).send({ error: "KAMAR LOGINS : `school`, `student_id` and `authkey` is required" });
+  }
+  if (!(req_data.school in dict_school_urls)) {
+    return res
+      .status(400)
+      .send({ error: `'${req_data.school}' is not valid. Valid schools are [${Object.keys(dict_school_urls)}]` });
+  }
+  var authconfig = {
+    method: "post",
+    url: dict_school_urls[req_data.school.toLowerCase()],
+    headers: kamarheaders,
+    data: `Command=GetStudentResults&Key=${req_data.authkey}&StudentID=${req_data.student_id}`,
+  };
+
+  await axios(authconfig)
+    .then(function (response) {
+      var responseObj = x2js.xml2js(response.data).StudentResultsResults;
+      var student_subjects = {};
+
+      if (responseObj.Error) {
+        return res.status(401).send({ error: responseObj.Error });
+      }
+
+      // Including not ue approved
+      let all_lvl3_results = responseObj.ResultLevels.ResultLevel.filter((r) => r.NCEALevel == 3)[0].Results.Result;
+
+      //Rankscore return
+      rankscorecalc = (g, c) => {
+        switch (g) {
+          case "Achieved with Excellence":
+            return 4 * parseInt(c);
+          case "Achieved with Merit":
+            return 3 * parseInt(c);
+          case "Achieved":
+            return 2 * parseInt(c);
+          default:
+            return 0;
+        }
+      };
+
+      // Only ue approved and correct subject added (unsorted)
+      unsorted_final_results = {};
+      all_lvl3_results.forEach((e) => {
+        let rs = rankscorecalc(e.Grade, e.Credits);
+        if (ue_approved_subjects[e.Number] in student_subjects) {
+          student_subjects[ue_approved_subjects[e.Number]][0] += rs;
+          student_subjects[ue_approved_subjects[e.Number]][1] += parseInt(e.CreditsPassed);
+        } else {
+          student_subjects[ue_approved_subjects[e.Number]] = [rs, parseInt(e.CreditsPassed)];
+        }
+        if (student_subjects[ue_approved_subjects[e.Number]][1] <= 24) {
+          unsorted_final_results[e.Number] = {
+            subject: ue_approved_subjects[e.Number],
+            rs_contribution: rs,
+            grade: e.Grade,
+            credits: e.CreditsPassed,
+            as_name: e.Title,
+            publish_date: e.ResultPublished,
+          };
+        }
+      });
+
+      res.status(200).send(unsorted_final_results);
+
+      //TODO 24 max, top 5, 80 best
+    })
+    .catch(function (error) {
+      console.log(error);
+      res.status(500).send({ error: "Server might be down, sorry :/ - from results" });
+    });
 });
 
 app.listen(PORT, () => {
